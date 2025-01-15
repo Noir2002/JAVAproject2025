bc7a25bb59397efc0a398b273d7e83f0c2c4412b
Mouzheng LI <L2164753957@gmail.com>
2025-01-15 18:33:13 +0100

    ok for managers' windows

--- src/main/java/com/isep/javafxdemo/test.java
+++ src/main/java/com/isep/javafxdemo/test.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 public class test {
     public static void main(String[] args) throws IOException {
         Kanban.setKanbanList();
-        /* 
-        Projet projet = new Projet(1, "projet1", "2025-01-01 00:00:00", 10000);
+        /*
+        Projet projet = new Projet(1, "projet1", "2025-01-01 00:00:00", 10000,1000);
         System.out.println("projet created: " + projet);
         Tache tache = new Tache(1, "tache1","2025-01-01 00:00:00", 1000, 0 , 1, "a faire", "description1", "commentaire1");
         System.out.println("tache created: " + tache);
@@ -28,7 +28,7 @@ public class test {
         }
 
         // Test DataHandler saveData
-        DataHandler.saveData();*/
+        DataHandler.saveData(); */
 
         /* 
         // Test DataHandler loadData
